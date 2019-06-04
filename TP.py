b343801065a67f310ad9cecc7e12d832e28b44af
Andres Seivane <andyseivane@hotmail.com>
2019-06-04 18:48:54 -0300

    Autocorrelacion andando

--- TP.py
+++ TP.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import *
-from scipy.fftpack import fft, fftshift
+from scipy.fftpack import fft, fftshift, ifft
 from scipy import signal
 
 BW_rg = 38e6
@@ -22,11 +22,12 @@ def tita(t, k1, k2):
 
 X = chirp_rg(tita(time, BW_rg/(2*T_rg),-BW_rg/2))
 
-#X=np.sin((2*np.pi/10)*ind)		#seno
+X1 = np.concatenate((X, zeros(len(X)-1,dtype=float)), axis=0, out=None)
+X1 = np.concatenate((zeros(len(X)-1,dtype=float),X1), axis=0, out=None)
 
-A = fft(X,2048)/(len(X)/2.0) 
-
-
+A1 = fft(np.flip(X1),2048)/(len(X1)/2.0) 
+A2 = fft(np.conjugate(X),2048)/(len(X)/2.0)
+'''
 plt.plot(np.real(X))
 #plt.plot(np.imag(X))
 plt.figure()
@@ -38,7 +39,27 @@ freq=np.linspace(-BW_rg/2,BW_rg/2,len(A))
 response = 20 * np.log10(np.abs(fftshift(A/abs(A).max())))
 plt.plot(freq,response)
 plt.xlabel("Frequency [cycles per sample]")
-#plt.plot(log10(ind/10),abs(DFT))	#seno
 plt.show()
+'''
 
 
+'''
+Auto correlacion de la chirp
+'''
+
+#A1_zeros = np.concatenate((A1, zeros(len(A1)-1,dtype=float)), axis=0, out=None)
+#A2_zeros = np.concatenate(A2,(zeros(len(A2)-1,dtype=float)), axis=0, out=None)
+A = ifft(A1*A2)
+'''
+plt.figure()
+plt.plot(X)
+
+plt.figure()
+plt.plot(X1)
+'''
+plt.figure()
+A=abs(A)
+A=A[500:]
+plt.plot(A)
+plt.xlabel("Autocorrelacion")
+plt.show()
